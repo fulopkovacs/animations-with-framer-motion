9b4f5b820a39c7431f97d99bf807bc37c30c3f0b
Fülöp Kovács <kovacs.fulop@gmail.com>
2023-01-08 09:03:07 +0100

    Init repo

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as ToggleGroup from '@radix-ui/react-toggle-group'
+function App() {
+  return (
+    <div className="App w-full h-full bg-gray-900 min-h-screen flex items-center justify-center gap-4">
+      <div className="text-red-300 font-bold">hello</div>
+    </div>
+  )
+}
+
+export default App
